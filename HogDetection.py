62c33a3cd12363af76f820f492ea97cd97a9c6d0
Sujit Varadhan <sujit5817@gmail.com>
2019-11-06 12:06:35 -0600

    refactor to take in single images

--- HogDetection.py
+++ HogDetection.py
@@ -7,43 +7,34 @@ import argparse
 import imutils
 import cv2
  
-def startHogDetection():
+def detect(path):
     # initialize the HOG descriptor/person detector
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     cv2.startWindowThread()
-    # open webcam video stream
-    cap = cv2.VideoCapture(0)
 
-    while(True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        # resizing for faster detection
-        frame = cv2.resize(frame, (640, 480))
-        # using a greyscale picture, also for faster detection
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        # detect people in the image
-        # returns the bounding boxes for the detected objects
-        boxes, weights = hog.detectMultiScale(frame, winStride=(5,5), padding=(8,8), scale=1.05)
-        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-        for (xA, yA, xB, yB) in boxes:
-            # display the detected boxes in the colour pictures
-            cv2.rectangle(frame, (xA, yA), (xB, yB),
-                            (0, 255, 0), 2)
-        boxes = np.array([[x, y, x+w, y+h] for (x,y,w,h) in boxes])
-        pick = non_max_suppression(boxes, probs=None, overlapThresh=0.65)
+    image = cv2.imread(path)
+    
+    image = imutils.resize(image, width=min(400, image.shape[1]))
 
-        for (xA, yA, xB, yB) in pick:
-            cv2.rectangle(frame, (xA, yA), (xB, yB),
-                            (0, 255, 0), 2)
-        
-        if (len(pick) > 0):
-            #send to updater 
+    boxes, weights = hog.detectMultiScale(image, winStride=(4,4), padding=(8,8), scale=1.05)
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour pictures
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                        (0, 255, 0), 2)
+    boxes = np.array([[x, y, x+w, y+h] for (x,y,w,h) in boxes])
+    pick = non_max_suppression(boxes, probs=None, overlapThresh=0.65)
 
-        # Display the resulting frame
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                        (0, 255, 0), 2)
+    
+    if (len(pick) > 0):
+        #send to updater 
+
+    # Display the resulting frame
+    cv2.imshow('image',image)
         
     # When everything done, release the capture
     cap.release()
