8669cbc2efacc477ad3876b40c6cf47fd362e95f
Sujit Varadhan <sujit5817@gmail.com>
2019-11-06 11:39:30 -0600

    make HOG detection call a function

--- HogDetection.py
+++ HogDetection.py
@@ -7,43 +7,48 @@ import argparse
 import imutils
 import cv2
  
-# initialize the HOG descriptor/person detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-cv2.startWindowThread()
-# open webcam video stream
-cap = cv2.VideoCapture(0)
+def startHogDetection():
+    # initialize the HOG descriptor/person detector
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    cv2.startWindowThread()
+    # open webcam video stream
+    cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # resizing for faster detection
-    frame = cv2.resize(frame, (640, 480))
-    # using a greyscale picture, also for faster detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(6,6), padding=(8,8), scale=1.05)
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-    for (xA, yA, xB, yB) in boxes:
-        # display the detected boxes in the colour picture
-        cv2.rectangle(frame, (xA, yA), (xB, yB),
-                          (0, 255, 0), 2)
-    boxes = np.array([[x, y, x+w, y+h] for (x,y,w,h) in boxes])
-    pick = non_max_suppression(boxes, probs=None, overlapThresh=0.65)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # resizing for faster detection
+        frame = cv2.resize(frame, (640, 480))
+        # using a greyscale picture, also for faster detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        # detect people in the image
+        # returns the bounding boxes for the detected objects
+        boxes, weights = hog.detectMultiScale(frame, winStride=(5,5), padding=(8,8), scale=1.05)
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+        for (xA, yA, xB, yB) in boxes:
+            # display the detected boxes in the colour pictures
+            cv2.rectangle(frame, (xA, yA), (xB, yB),
+                            (0, 255, 0), 2)
+        boxes = np.array([[x, y, x+w, y+h] for (x,y,w,h) in boxes])
+        pick = non_max_suppression(boxes, probs=None, overlapThresh=0.65)
 
-    for (xA, yA, xB, yB) in pick:
-        cv2.rectangle(frame, (xA, yA), (xB, yB),
-                          (0, 255, 0), 2)
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(frame, (xA, yA), (xB, yB),
+                            (0, 255, 0), 2)
+        
+        if (len(pick) > 0):
+            #send to updater 
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-	
-# When everything done, release the capture
-cap.release()
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    # When everything done, release the capture
+    cap.release()
 
-# finally, close the window
-cv2.destroyAllWindows()
-cv2.waitKey(1)
+    # finally, close the window
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+
